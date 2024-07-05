024946c8522b453f63d0d8069d32840a58022807
unknown <aminkazzout@gmail.com>
2024-07-05 12:09:53 +0100

    fixed product cards

--- assets/js/vente.js
+++ assets/js/vente.js
@@ -1,5 +1,33 @@
 import $ from 'jquery';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-$(window).on('load', function() {
-    $('#infoPatient').modal('show');
+import { Modal } from 'bootstrap/dist/js/bootstrap.bundle.js';
+
+
+$(document).ready(function() {
+    let selectedCard;
+
+    $('.produit').click(function() {
+        selectedCard = $(this);
+    });
+
+    $('#confirmQuantity').click(function() {
+        const quantity = $('#quantityInput').val();
+        if (quantity && selectedCard) {
+            const quantityBadge = selectedCard.find('.quantity-badge');
+            quantityBadge.text(quantity);
+            quantityBadge.removeClass('d-none');
+            
+           $('#qteModal').modal('hide')
+            // let modal = new Modal($('#qteModal'));
+            // console.log(modal)
+            // modal.hide();
+        }else if(quantity==''){
+            const quantityBadge = selectedCard.find('.quantity-badge');
+            quantityBadge.text(quantity);
+            quantityBadge.addClass('d-none');
+
+            const modal = new Modal(document.getElementById('qteModal'));
+           
+            modal.hide();
+        }
+    });
 });
